e6e50958d0232054c63d43dacf444eca2ba6e78d
ROY Anne-Marie <royanne-marie@ROYs-MacBook-Pro.local>
2024-12-08 21:39:42 +0100

    Remove font imports to fix build error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-import '@fontsource/source-serif-pro/400.css';
-import '@fontsource/source-serif-pro/700.css';
 import '../src/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
--- pages/character-creator.js
+++ pages/character-creator.js
@@ -1,1 +1,15 @@
- 
+import React from 'react'
+
+function CharacterCreator() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Character Creator</h1>
+      <div>
+        {/* Add your character creation form/content here */}
+        <p>Character creation interface coming soon...</p>
+      </div>
+    </div>
+  )
+}
+
+export default CharacterCreator
